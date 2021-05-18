932b7b94975a61417a338b3a5a3fbcae349e7f75
gonta1026 <katano.original@gmail.com>
2021-05-18 23:00:21 +0900

    モーダルコンポーネントを追加

---
+++ src/components/atoms/BasicDialog.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-dialog
+    :visible.sync="visible"
+    :title="title"
+    :before-close="handleClose"
+    :width="width"
+  >
+    <slot />
+  </el-dialog>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {
+  },
+  props: {
+    visible: {
+      type: Boolean,
+      default: false,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    width: {
+      type: String,
+      default: '30%'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  methods: {
+    handleClose () {
+      this.$emit('handleClose', false)
+    }
+  }
+})
+
+</script>
